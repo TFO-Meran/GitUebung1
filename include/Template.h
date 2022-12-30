83ad9994377edc9826fc6ba51b2122dffef4faa9
cg <c.g.mail@gmx.net>
2022-12-30 17:55:44 +0100

    uebung 1 eingefügt

--- include/Template.h
+++ include/Template.h
@@ -1,6 +1,13 @@
 
 #pragma once
-
+/******************************************************************************
+ *  @file       [filename].h
+ *  @version    [Vxx.yy.zz]	
+ *  @brief      [brief description of the content]
+ *  @author     [Author-Name]
+ *  Website     (optional) [https://... ]
+ *  Support     (optional) [xxx@yyy.zzz]
+ ******************************************************************************/
 
 // ============================================================================
 //                           END OF FILE
