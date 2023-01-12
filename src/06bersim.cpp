e072bd8c1cb36f6b2166cc86de6a2de5c900d661
06bersim <06bersim@rgtfo-me.it>
2023-01-12 09:03:35 +0100

    git commit 3 simon

--- src/06bersim.cpp
+++ src/06bersim.cpp
@@ -28,7 +28,7 @@ int main(int, char **)
         cout << "Gib deinen Benutzer ein ";
         cin >> ben;
         test = get_06bersim(ben);
-        //test
+        //test 3
     } while (test==true);
 
 }
